cbbebce7640a00c6c8425b19409b8eab15dadf08
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-03-01 14:28:47 +0200

    CRUD For Profit Goal Finished Working

--- app/Http/Controllers/ProfitgoalController.php
+++ app/Http/Controllers/ProfitgoalController.php
@@ -4,8 +4,62 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Profitgoal;
+use App\Models\Admin;
+
 
 class ProfitgoalController extends Controller
 {
-    //
+    public function addProfitgoal(Request $request){
+        $profitgoal = new Profitgoal ;
+               $netProfit = $request->input('netProfit');
+               $isdeleted = $request->input('isdeleted');
+           $profitgoal->netProfit = $netProfit;
+           $profitgoal->isdeleted = $isdeleted;
+           $profitgoal->save();
+           return response()->json([
+               'message'=>$profitgoal,
+           ]);
+}
+
+public function getProfitgoal(Request $request, $id ){
+    $profitgoal = Profitgoal::where('id',$id)->get();
+
+    return response()->json([
+        'message' => $profitgoal,
+    ]);
+
+}
+
+public function getProfitgoalAll(Request $request){
+    $profitgoal = Profitgoal::get();
+
+    return response()->json([
+        'message' => $profitgoal,
+    ]);
+
+}
+
+
+
+public function editProfitgoal(Request $request, $id ){
+    $profitgoal = Profitgoal::find($id);
+    $inputs=$request->except('isdeleted','_method');
+    $profitgoal->isdeleted = $request->input('isdeleted');
+    $profitgoal->update($inputs);
+
+    return  response()->json([
+        'message' =>'Profitgoal edited successtully',
+        'Profitgoal' =>$profitgoal,
+    ]);
+
+    }
+
+    public function deleteProfitgoal(Request $request, $id){
+        $profitgoal = Profitgoal::find($id);
+        $profitgoal->delete();
+        return response()->json([
+            'message' =>'Profitgoal deleted successtully',
+            'Profitgoal' =>$profitgoal,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\RecurringController;
+use App\Http\Controllers\ProfitgoalController;
+
 
 
 /*
@@ -35,3 +37,13 @@ Route::Patch('/recurring/{id}',[RecurringController::class,'editRecurring']);
 
 Route::Delete('/recurring/{id}',[RecurringController::class,'deleteRecurring']);
 
+// CRUD For Profitgoal
+Route::Post('/profitgoal',[ProfitgoalController::class,'addProfitgoal']);
+
+Route::Get('/profitgoal/{id}',[ProfitgoalController::class,'getProfitgoal']);
+
+Route::Get('/profitgoal',[ProfitgoalController::class,'getProfitgoalAll']);
+
+Route::Patch('/profitgoal/{id}',[ProfitgoalController::class,'editProfitgoal']);
+
+Route::Delete('/profitgoal/{id}',[ProfitgoalController::class,'deleteProfitgoal']);
