402bd669ce9a305af739f64fbbb6908395467fa0
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-03-03 08:06:01 +0200

    merged admin from another branch

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,24 +1,98 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-   public function addAdmin(Request $request){
-$admin = new Admin;
-$username = $request->input('username');
-$password = $request->input('password');
-//$isdeleted= $request->isdeleted=='off' ? 0 : 1;
-//$issuper = $request->isSuper=='off' ? 0 : 1;
-$admin->username=$username;
-$admin->password=$password;
-// $admin->isDeleted=$isdeleted;
-// $admin->isSuper=$issuper;
-$admin->save();
-return ; return response()->json([
-    'message' => '$username created successfully',
-]);
-   }
+    protected function respondWithToken($token){
+        $expiration = Auth::guard('admin')->factory()->getTTL() * 60; // TTL in seconds
+        $response = response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $expiration,
+        ]);
+        $response->withCookie($this->setTokenCookie($token));
+        return $response;
+    }
+
+    protected function setTokenCookie($token){
+        $expiration = Auth::guard('admin')->factory()->getTTL() * 60; // TTL in seconds
+        return cookie('token', $token, $expiration, null, null, false, true);
+    }
+
+    public function register(Request $request){
+        $type = $request->type;
+        $user=Admin::create([
+            'name' =>$request->name,
+            'password' =>$request->password,
+            'email' =>$request->email,
+            'type'=> $type ? $type : 0,
+        ]);
+        $token = Auth::guard('admin')->login($user);
+        $response = $this->respondWithToken($token);
+        return $response;
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $userType = $user->type; // retrieve the user's type from the database
+            $token = $user->createToken('authToken')->accessToken;
+
+            if($userType == '1') {
+                return response()->json([
+                    'message' => 'Success! You will be redirected to the clickable link.',
+                "token" => $token,
+                'userType' => $userType,
+                ]);
+            } else {
+                return response()->json(['access_token' => $token, 'user_type' => $userType]);
+            }
+        } else {
+            return response()->json(['error' => 'Invalid email or password'], 401);
+        }
+    }
+
+    public function logout(){
+        Auth::guard('admin')->logout();
+        $response = response()->json(['message' => 'Successfully logged out.']);
+        $response->withCookie(cookie('token', null, -1, null, 'localhost:8000'));
+        return $response;
+    }
+
+    public function allUsers(){
+        $users = Admin::all();
+        return response()->json($users);
+    }
+
+    public function update(Request $request, $id){
+        $user = Admin::find($id);
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->name = $request->input('name');
+        $user->password = $request->input('password');
+        $user->email = $request->input('email');
+        $user->type = $request->input('type') ?? $user->type;
+        $user->save();
+
+        return response()->json(['message' => 'User updated successfully']);
+    }
+
+    public function delete($id){
+        $user = Admin::find($id);
+        if ($user) {
+            $user->delete();
+            return response()->json(['message' => 'User deleted successfully.']);
+        } else {
+            return response()->json(['error' => 'User not found.'], 404);
+        }
+    }
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,19 +2,60 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Admin extends Model
+class Admin extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use HasApiTokens, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-        'username',
+        'name',
+        'email',
         'password',
-        // 'isDeleted',
-        // 'isSuper',
+        'type',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+    public function setPasswordAttribute($password){
+        if(!empty($password)){
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
 
     public function profitgoal(){
         return $this->hasOne(Profitgoal::class);
@@ -29,3 +70,4 @@ class Admin extends Model
     }
 
 }
+
